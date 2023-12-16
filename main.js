b59d603f5e64fd86eef711289098da22717b3611
ALauraOliva <andrea_carol00@hotmail.com>
2023-12-15 19:00:33 -0500

    accordion style

--- main.js
+++ main.js
@@ -31,6 +31,23 @@ option2.addEventListener("click", () => {
   changeOption();
 });
 
+//?Accordion
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
+
 //?Smooth Scroll Behavior
 
 document.addEventListener("DOMContentLoaded", () => {
